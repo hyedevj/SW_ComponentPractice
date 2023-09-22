8ed221d147ebec154289294a836ed8ec318bdaa9
hyedevj <hyedevj@gmail.com>
2023-09-22 14:48:39 +0900

    사용자 정의 Hook 연습하기 - useClickAway

---
+++ src/hooks/useClickAway.js
@@ -0,0 +1,36 @@
+import { useEffect, useRef } from "react"
+
+const events = ['mousedown', 'touchstart']
+
+const useClickAway = (handler) => {
+  const ref = useRef(null)
+  // 퍼포먼스 개선
+  const saveHandler = useRef(handler)
+
+  useEffect(() => {
+    saveHandler.current = handler
+  }, [handler])
+
+  useEffect(() => {
+    const element = ref.current
+    if (!element) return
+
+    const handleEvent = (e) => {
+      !element.contains(e.target) && saveHandler.current(e)
+    }
+
+    for (const eventName of events) {
+      document.addEventListener(eventName, handleEvent)
+    }
+
+    return () => {
+      for (const eventName of events) {
+        document.removeEventListener(eventName, handleEvent)
+      }
+    }
+  }, [ref])
+
+  return ref
+}
+
+export default useClickAway
